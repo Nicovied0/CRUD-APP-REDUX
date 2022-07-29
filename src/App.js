c5dfed7f5fd8da983c29c460abc07b9e4e1d5bc8
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-07-29 14:42:31 -0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const tasksState = useSelector(state => state.task)
+
   return (
     <div className="App">
-     <h2>HOLAS</h2>
+     <h2>HOlas</h2>
     </div>
   );
 }
---
+++ src/features/task/tasksSlice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const taskSlice = createSlice({
+    name:'tasks',
+    initialState:[],
+})
+
+export default taskSlice.reducer
